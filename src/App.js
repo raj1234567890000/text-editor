688546e8b343b353022c2c9bf60bbfbe9e03f281
Rohit Rajput <rajputrohitbcs@gmail.com>
2024-07-20 12:07:57 +0530

    text-editor

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import TextEditor from "./components/TextEditor";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Text Editor</h1>
+      </header>
+      <div className="editor">
+        <TextEditor />
+      </div>
+    </div>
+  );
+}
+
+export default App;
